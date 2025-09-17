038b98d9cce242fd7f0e88d901c60a6e0b21920e
Игорь Шеповалов <shepovalov.igor@mail.ru>
2025-09-17 15:01:37 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hydro_losses_lib',
+    version='0.1.0',
+    description='Библиотека для расчета гидравлических потерь в гидравлических элементах',
+    author='Igor Shepovalov',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
